b8705229d50344450bc5c4097af660821ef614e2
Yoshiyoshida <yoshiyoshida0213@gmail.com>
2018-10-28 11:32:14 +0900

    追加

--- hello/urls.py
+++ hello/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    #nameは{% url %}タグで利用できる
     path('', views.index, name='index'),
     path('next', views.next, name='next'),
 ]
