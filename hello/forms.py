6feece1b2b4696c6b641d85a53cb59bfb8d8fbbd
Yoshiyoshida <yoshiyoshida0213@gmail.com>
2018-12-31 17:55:29 +0900

    add

--- hello/forms.py
+++ hello/forms.py
@@ -4,3 +4,5 @@ class HelloForm (forms.Form):
     name = forms.CharField(label = 'name')
     mail = forms.CharField(label = 'mail')
     age = forms.IntegerField(label = 'age')
+    #CharField:テキスト用フィールド
+    #IntergerFiled : 整数用フィールド
--- hello/views.py
+++ hello/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse#クライアントに返す内容を管理するクラス
-
+from .forms import  HelloForm
 
 def index(request):#requestはHTTPRequestクラスのインスタンス
     params = {
             'title' : 'Hello/Index',
             'msg' : 'お名前は?',
+            'form' : HelloForm()
     }
-    return render(request, 'hello/index.html', params)
             #render(httpRequest, テンプレート)
     #if 'msg' in request.GET:#request.GETは辞書のキー
         #msg = request.GET['msg']#msgはキーに対する値
+
+    if (request.method == 'post'):
+        params['message'] = '名前' + request.POST['name'] + '<br>メール: ' + request.POST['mail'] + '<br>年齢: ' + request.POST['age']
+        params['form'] = HelloForm(request.POST)
+
+    return render(request, 'hello/index.html', params)
+
 
 def form (request):
     msg = request.POST['msg']
